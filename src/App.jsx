a0ca73bc422e48699f77d6053acad6d867ca4add
zarskyb <mike.bazarsky@gmail.com>
2024-01-19 15:50:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./app.scss"
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <div>
+      <section>
+        <Navbar/>
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Portfolio</section>
+      <section>Contact</section>
+    </div>
+  );
+}
+
+export default App;
